1f64141f067c19624948ebd18ebeb7980c79dbdf
Bastien ADAM <badam@e3r12p6.42.fr>
2019-10-07 16:18:11 +0200

    Not tested

---
+++ ft_memccpy.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 15:44:54 by badam             #+#    #+#             */
+/*   Updated: 2019/10/07 16:17:19 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdlib.h"
+#include "ft_strlen.h"
+
+void	*ft_memccpy(unsigned char *dst, const unsigned char *src,
+		int c, size_t n)
+{
+	while (*src && n-- && *src != c)
+		*(dst++) = *((unsigned char*)(src++));
+	return (*src ? *src : NULL);
+}
