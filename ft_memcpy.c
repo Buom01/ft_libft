01b4aa9f763cf64a198104fb6cd093788c403c0c
Bastien ADAM <badam@e3r12p6.42.fr>
2019-10-07 15:42:46 +0200

    Not tested

---
+++ ft_memcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 14:38:41 by badam             #+#    #+#             */
+/*   Updated: 2019/10/07 15:41:50 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdlib.h"
+#include "ft_strlen.c"
+
+void	*ft_memcpy(unsigned char* dst, const unsigned char* src, size_t n)
+{
+	unsigned char *dstbgn;
+	unsigned char *dstcpy;
+	unsigned char *dstcpybgn;
+
+	dstbgn = dst;
+	dstcpy = malloc(ft_strlen((const char*)dst) * sizeof(*dst));
+	dstcpybgn = dstcpy;
+	while (*dst)
+		*(dstcpy++) = *(dst++);
+	dst = dstbgn;
+	while (*src && n--)
+		*(dst++) = *((unsigned char*)(src++));
+	return (dstcpybgn);
+}
