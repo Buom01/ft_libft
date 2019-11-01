ede5e48a8940d0b392b10f7ad464ba1f2712414f
Bastien ADAM <badam@e1r2p7.42.fr>
2019-11-01 16:27:11 +0100

    Not tested

---
+++ ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/30 15:16:44 by badam             #+#    #+#             */
+/*   Updated: 2019/11/01 16:26:39 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stddef.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char	*hscpy;
+	size_t	ndcur;
+
+	hscpy = (char*)haystack;
+	ndcur = 0;
+	while (needle[ndcur] && *hscpy && ndcur < len)
+	{
+		if (needle[ndcur] == *(hscpy++))
+			ndcur++;
+		else
+		{
+			hscpy = hscpy - ndcur;
+			ndcur = 0;
+		}
+	}
+	if (needle[ndcur] && ndcur < len && ndcur > 0)
+		return (--hscpy);
+	else
+		return (NULL);
+}
