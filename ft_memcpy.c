e4e6a2accbc8a1774d22ba440b6392e5b6b32577
Bastien ADAM <badam@e2r13p9.42.fr>
2019-11-04 17:48:24 +0100

    Minor change

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:38:41 by badam             #+#    #+#             */
-/*   Updated: 2019/11/04 16:35:37 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/04 17:44:23 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	dstcpy = dst;
 	srccpy = (void*)src;
 	while (n--)
-		*((unsigned char*)dstcpy++) = *((unsigned char*)(srccpy++));
+		*((unsigned char*)(dstcpy++)) = *((unsigned char*)(srccpy++));
 	return (dst);
 }
