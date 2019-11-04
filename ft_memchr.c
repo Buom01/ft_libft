f06b157c36039aeafe26f149377f7178387e3a31
Bastien ADAM <bastien.67880@gmail.com>
2019-11-04 01:27:14 +0100

    Cleanify (not tested)

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 16:29:15 by badam             #+#    #+#             */
-/*   Updated: 2019/11/01 16:37:21 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/04 01:26:54 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*hscpy;
-	unsigned char	needle;
-	size_t			cursor;
+	unsigned char	*strcpy;
+	size_t			cur;
 
-	hscpy = (unsigned char*)s;
-	needle = (unsigned char)c;
-	cursor = 0;
-	while (*hscpy && cursor++ < n)
-		if (*(hscpy++) == c)
-			return (--hscpy);
+	strcpy = (unsigned char*)s;
+	cur = 0;
+	while (*strcpy && cur++ < n)
+		if (*(strcpy++) == c)
+			return (--strcpy);
 	return (NULL);
 }
