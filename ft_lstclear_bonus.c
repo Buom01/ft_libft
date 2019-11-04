6ec73492650371a83a19db3fa4281427d71c8398
Bastien ADAM <badam@e2r13p9.42.fr>
2019-11-04 17:09:34 +0100

    Not tested

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 17:03:49 by badam             #+#    #+#             */
+/*   Updated: 2019/11/04 17:09:16 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_lstdelone_bonus.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	void	*next;
+	next = *lst;
+	while (next)
+	{
+		ft_lstdelone(next, del);
+		next = (*lst)->next;
+	}
+}
