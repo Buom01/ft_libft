e7259e4d37331f300c42f9cef2f712b36f69d721
Bastien ADAM <badam@e2r13p9.42.fr>
2019-11-04 15:59:58 +0100

    Not tested

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 15:51:20 by badam             #+#    #+#             */
+/*   Updated: 2019/11/04 15:59:05 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unistd.h"
+
+t_list *ft_lstnew(void *content)
+{
+	t_list	*lst;
+
+	if (!(lst = malloc(sizeof(ft_lstnew))))
+		return (NULL);
+	lst->content = content;
+	lst->next = NULL;
+	return (lst);
+}
