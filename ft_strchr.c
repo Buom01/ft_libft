65e58011d5cc9fcf4b483cedf1dc48dc6a6076eb
Bastien ADAM <badam@e2r13p9.42.fr>
2019-11-04 18:46:14 +0100

    Minor patch

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 15:23:08 by badam             #+#    #+#             */
-/*   Updated: 2019/11/04 18:42:12 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/04 18:44:51 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	strcpy = (unsigned char*)s;
 	while (*strcpy != (unsigned char)c)
 	{
-		strcpy++;
 		if (!(*strcpy))
 			break;
+		strcpy++;
 	}
 	return (((unsigned char)c == *strcpy) ? strcpy : NULL);
 }
