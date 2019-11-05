004d7bdf884ce9247d203d310885a7d7c5ca65fa
Bastien ADAM <badam@e2r7p8.42.fr>
2019-11-05 17:17:48 +0100

    Review

--- ft_split.c
+++ ft_split.c
@@ -6,27 +6,47 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/03 19:15:56 by badam             #+#    #+#             */
-/*   Updated: 2019/11/03 19:39:30 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/05 17:17:16 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdlib.h"
 #include "ft_strchr.h"
-#include "ft_strrchr.h"
+#include "ft_strlcpy.h"
+
+char	**ft_split_free(char **tab, size_t ntofree)
+{
+	while (ntofree--)
+		free(*(tab + ntofree));
+	free(tab);
+	return (NULL);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
+	char	**tabcpy;
+	size_t	strc;
+	char	*strcpy;
+	size_t	substrlen;
 
-	tab = malloc(sizeof(char*) * 3);
-	if (!tab)
+	strc = 0;
+	strcpy = (char*)s;
+	while (*strcpy)
+		if (*(strcpy++) == c)
+			strc++;
+	if (!(tab = malloc(sizeof(char*) * (++strc + 1))))
 		return (NULL);
-	tab[0] = ft_strchr(s, (int)c);
-	if (!tab[0])
-		return (NULL);
-	tab[1] = ft_strrchr(s, (int)c);
-	if (!tab[1])
-		return (NULL);
-	tab[2] = NULL;
+	strcpy = (char*)s;
+	tabcpy = tab;
+	while (--strc)
+	{
+		substrlen = ft_strchr(strcpy, (int)c) - strcpy;
+		if (!ft_strlcpy((*(tabcpy++) = malloc(substrlen * sizeof(char))),
+				(const char*)strcpy, substrlen))
+			return (ft_split_free(tab, tabcpy - tab - 1));
+		strcpy += substrlen;
+	}
+	*tabcpy = NULL;
 	return (tab);
 }
