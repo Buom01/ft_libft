46d5b1a8ca3d541d9cea6104ac686d31abebcdad
Bastien ADAM <badam@e1r13p5.42.fr>
2019-11-05 20:31:00 +0100

    Rewrite

--- ft_substr.c
+++ ft_substr.c
@@ -6,32 +6,29 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 17:46:03 by badam             #+#    #+#             */
-/*   Updated: 2019/11/01 18:01:54 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/05 20:30:24 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdlib.h"
+#include "ft_strlen.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*strcpy;
-	size_t	newstrlen;
-	char	*newstr;
-	char	*newstrcpy;
+	size_t	newlen;
+	char	*substr;
+	char	*substrcpy;
 
-	strcpy = (char*)(s + start);
-	newstrlen = 0;
-	while (*(strcpy++))
-		newstrlen++;
-	if (len < newstrlen)
-		newstrlen = len;
-	newstr = malloc(newstrlen + 1);
-	if (!newstr)
+	strcpy = (char*)s + start;
+	newlen = ft_strlen(strcpy);
+	if (newlen > len)
+		newlen = len;
+	if (!(substr = malloc((newlen + 1) * sizeof(char))))
 		return (NULL);
-	strcpy = (char*)(s + start);
-	newstrcpy = strcpy;
-	while (newstrlen--)
-		*(newstrcpy++) = *(strcpy++);
-	*newstrcpy = '\0';
-	return (newstr);
+	substrcpy = substr;
+	while (newlen--)
+		*(substr++) = *(strcpy++);
+	*substr = '\0';
+	return (substrcpy);
 }
