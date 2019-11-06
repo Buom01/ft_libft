ebe1f415ef759aa9a57df6eadd5f90f57b04be90
Bastien ADAM <badam@e2r4p23.42.fr>
2019-11-06 18:15:34 +0100

    Rewrite, something running

--- ft_split.c
+++ ft_split.c
@@ -5,48 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/03 19:15:56 by badam             #+#    #+#             */
-/*   Updated: 2019/11/05 17:17:16 by badam            ###   ########.fr       */
+/*   Created: 2019/11/06 16:36:27 by badam             #+#    #+#             */
+/*   Updated: 2019/11/06 18:14:32 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "stddef.h"
 #include "stdlib.h"
-#include "ft_strchr.h"
-#include "ft_strlcpy.h"
-
-char	**ft_split_free(char **tab, size_t ntofree)
-{
-	while (ntofree--)
-		free(*(tab + ntofree));
-	free(tab);
-	return (NULL);
-}
+#include "ft_substr.h"
 
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
-	char	**tabcpy;
-	size_t	strc;
 	char	*strcpy;
-	size_t	substrlen;
+	size_t	frgmt;
+	size_t	frgmtlen;
 
-	strc = 0;
+	frgmt = 0;
 	strcpy = (char*)s;
-	while (*strcpy)
-		if (*(strcpy++) == c)
-			strc++;
-	if (!(tab = malloc(sizeof(char*) * (++strc + 1))))
+	while (*(strcpy++))
+		if (*(strcpy - 1) == c && *(strcpy - 2) != c)
+			frgmt++;
+	if (!(tab = malloc((frgmt++ + 1) * sizeof (char*))))
 		return (NULL);
 	strcpy = (char*)s;
-	tabcpy = tab;
-	while (--strc)
+	while (*strcpy)
 	{
-		substrlen = ft_strchr(strcpy, (int)c) - strcpy;
-		if (!ft_strlcpy((*(tabcpy++) = malloc(substrlen * sizeof(char))),
-				(const char*)strcpy, substrlen))
-			return (ft_split_free(tab, tabcpy - tab - 1));
-		strcpy += substrlen;
+		frgmtlen = 0;
+		while (strcpy[frgmtlen] && strcpy[frgmtlen] != c)
+			frgmtlen++;
+		if (!(*(tab++) = ft_substr(strcpy, 0, frgmtlen)))
+		{
+			// Free up
+			return (NULL);
+		}
+		strcpy += frgmtlen + 1;
+		while (*strcpy && *strcpy == c)
+			strcpy++;
 	}
-	*tabcpy = NULL;
-	return (tab);
+	*tab = (NULL);
+	return (tab - frgmt);
 }
