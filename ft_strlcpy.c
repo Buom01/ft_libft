5727b5223efef8df0e6550d5c3ffa77c11497c60
Bastien ADAM <badam@e2r11p11.42.fr>
2019-11-07 17:35:23 +0100

    Manage string end

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 16:11:28 by badam             #+#    #+#             */
-/*   Updated: 2019/11/05 21:09:59 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/07 17:35:00 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		return (0);
 	while (*srccpy && --dstsize > 0)
 		*(dstcpy++) = *(srccpy++);
-	if (dstsize > 0)
+	if (++dstsize > 0)
 		*dstcpy = '\0';
 	return (ft_strlen(src));
 }
