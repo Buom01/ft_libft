45041651c8dd9d60f8ae22ea8b2ac5294612e026
Bastien ADAM <badam@e2r11p11.42.fr>
2019-11-07 21:25:19 +0100

    Test and norm

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 16:11:28 by badam             #+#    #+#             */
-/*   Updated: 2019/11/07 17:35:00 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/07 21:24:41 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	srccpy = (char*)src;
 	if (!dst || !src)
 		return (0);
-	while (*srccpy && --dstsize > 0)
-		*(dstcpy++) = *(srccpy++);
-	if (++dstsize > 0)
+	if (dstsize)
+	{
+		while (*srccpy && dstsize && --dstsize)
+			*(dstcpy++) = *(srccpy++);
 		*dstcpy = '\0';
+	}
 	return (ft_strlen(src));
 }
