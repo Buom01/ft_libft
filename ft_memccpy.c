f237f3d401bac46159982b394c8c6cac8a3ab11c
Bastien ADAM <badam@e1r1p4.42.fr>
2019-11-10 20:26:39 +0100

    Rewrite

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,22 +6,23 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 15:44:54 by badam             #+#    #+#             */
-/*   Updated: 2019/11/10 20:17:39 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/10 20:21:43 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdlib.h"
 #include "ft_strlen.h"
 
-void	*ft_memccpy(char *dst, const char *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t	i;
+	unsigned char	*srccpy;
+	unsigned char	*dstcpy;
+	unsigned char	ccpy;
 
-	i = 0;
-	while (n-- && src[i - 1] != c)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	return (src[i - 1] == c ? (void*)(dst) : NULL);
+	dstcpy = (unsigned char*)dst;
+	srccpy = (unsigned char*)src;
+	ccpy = (unsigned char)c;
+	while (n-- && *(srccpy - 1) != ccpy)
+		*(dstcpy++) = *(srccpy++);
+	return (*(srccpy - 1) == ccpy ? (void*)(dstcpy) : NULL);
 }
