2b7466cd1768791d806d5209d66da22eb2d86308
Bastien ADAM <badam@e1r1p4.42.fr>
2019-11-10 19:39:18 +0100

    Rewrite/Mix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,40 +6,31 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 14:16:46 by badam             #+#    #+#             */
-/*   Updated: 2019/11/09 21:08:30 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/10 19:36:38 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stddef.h"
 #include "ft_strlen.h"
-#include "stdio.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	char	*dstcpy;
 	char	*srccpy;
-	size_t	len;
+	size_t	dstlen;
+	size_t	srclen;
+	size_t	dstsizecpy;
 
 	dstcpy = (char*)dst;
 	srccpy = (char*)src;
-	len = ft_strlen(dstcpy);
-	if (!dstsize && !ft_strlen(srccpy))
-		return (0);
-	dstcpy += len;
-	if (len <= dstsize)
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (dstsize && (dstsizecpy = dstsize - dstlen) <= dstsize && dstsizecpy)
 	{
-		dstsize = ((size_t)(dstsize - len) > dstsize) ? (0) : (dstsize - len);
-		while (*srccpy && dstsize && --dstsize && ++len)
-		{
-			printf("\n%s; ", srccpy);
+		dstcpy = dstcpy + dstlen;
+		while (*srccpy && dstsizecpy && --dstsizecpy)
 			*(dstcpy++) = *(srccpy++);
-		}
-		if (dstsize)
-			*dstcpy = '\0';
-		while (*(srccpy++))
-			len++;
+		*dstcpy = '\0';
 	}
-	else
-		len += dstsize;
-	return (len);
+	return ((dstsize < dstlen) ? dstsize + srclen : dstlen + srclen);
 }
