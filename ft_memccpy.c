7642ccdfed2eb24c3ecb058658144c68b7c1fad6
Bastien ADAM <badam@e1r2p4.42.fr>
2019-11-12 20:09:26 +0100

    Proper conditions

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 15:44:54 by badam             #+#    #+#             */
-/*   Updated: 2019/11/12 20:05:51 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/12 20:09:08 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	unsigned char	*srccpy;
+	unsigned char	*srccpybgn;
 	unsigned char	*dstcpy;
 	unsigned char	ccpy;
 
 	dstcpy = (unsigned char*)dst;
 	srccpy = (unsigned char*)src;
+	srccpybgn = (unsigned char*)src;
 	ccpy = (unsigned char)c;
-	if (n)
+	while (n-- && (srccpy == srccpybgn || *(srccpy - 1) != ccpy))
 		*(dstcpy++) = *(srccpy++);
-	while (n-- && *(srccpy - 1) != ccpy)
-		*(dstcpy++) = *(srccpy++);
-	return (*(srccpy - 1) == ccpy ? (void*)(dstcpy) : NULL);
+	return ((srccpy != srccpybgn && *(srccpy - 1) == ccpy) ? (void*)(dstcpy) : NULL);
 }
