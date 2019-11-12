78f752891aa82fa55eb6d5c6bf469e3399b3b68f
Bastien ADAM <badam@e1r2p4.42.fr>
2019-11-12 19:40:44 +0100

    Patch limit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 15:51:34 by badam             #+#    #+#             */
-/*   Updated: 2019/11/04 18:48:33 by badam            ###   ########.fr       */
+/*   Updated: 2019/11/12 19:40:12 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	s3 = (unsigned char*)s1;
 	s4 = (unsigned char*)s2;
-	while (*s3 == *s4 && *s3 && (n--) > 0)
+	while (*s3 == *s4 && *s3 && n && (--n) > 0)
 	{
 		s3++;
 		s4++;
