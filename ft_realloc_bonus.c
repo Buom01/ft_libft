74d40f9f4c40fcc0a1b41f08463ab4a8ec018c9b
Bastien ADAM <bastien.67880@gmail.com>
2020-04-10 23:16:09 +0200

    Norminette

--- ft_realloc_bonus.c
+++ ft_realloc_bonus.c
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/10 22:55:29 by badam             #+#    #+#             */
-/*   Updated: 2020/04/10 23:03:56 by badam            ###   ########.fr       */
+/*   Updated: 2020/04/10 23:15:46 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,12 @@ void	*ft_realloc(void *ptr, size_t dstsize, size_t srcsize)
 
 	if (dstsize)
 	{
-		if ( !(ptr2 = malloc(dstsize)) )
+		if (!(ptr2 = malloc(dstsize)))
 			return (NULL);
 		if (ptr)
-			ft_memcpy( ptr2, ptr, (srcsize <= dstsize ? srcsize : dstsize ) );
+			ft_memcpy(ptr2, ptr, (srcsize <= dstsize ? srcsize : dstsize));
 	}
 	if (ptr)
 		free(ptr);
 	return (ptr2);
 }
-
-
