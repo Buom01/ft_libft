af9f466f6434ad02aaf6103313990025cdd82a00
Bastien ADAM <badam@student.42.fr>
2023-03-06 00:49:32 +0100

    Fix wrong malloc size

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:51:20 by badam             #+#    #+#             */
-/*   Updated: 2021/12/01 23:30:55 by bastien          ###   ########.fr       */
+/*   Updated: 2023/03/06 00:42:14 by bastien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*lst;
 
-	lst = malloc(sizeof(ft_lstnew));
-	if (!lst)
+	if (!(lst = malloc(sizeof(t_list))))
 		return (NULL);
 	lst->content = content;
 	lst->next = NULL;
